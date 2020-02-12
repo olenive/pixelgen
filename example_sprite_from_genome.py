60cbcf8b7b60267b455db160f52667b72a217061
Oleg Lenive <10035315+olenive@users.noreply.github.com>
2020-02-12 15:58:16 +0000

    Draw a surface and set different alpha values in example_sprite_from_genome.py

--- example_sprite_from_genome.py
+++ example_sprite_from_genome.py
@@ -6,6 +6,12 @@ import numpy as np
 import neat
 import pygame
 from pygame import surfarray
+from typing import List
+
+from helpers.image import ImageIO, ImageConvert
+
+
+SPRITE_DIMENSIONS = (32, 12)
 
 
 """Create a neural network from a genome & config."""
@@ -29,32 +35,46 @@ net = neat.nn.FeedForwardNetwork.create(genome, config)
 
 
 """Feed in input and obtain output from neural network."""
-net_output = net.activate((1, 1))
+net_output: List = net.activate((1, 1))
 
 
 """Make a sprite from the NN's output."""
+reshaped_net_output = np.reshape(np.array(net_output), SPRITE_DIMENSIONS)
+palette = ((200, 0, 0, 100), (100, 0, 100, 100), (0, 0, 200, 200), (0, 200, 0, 100))
+rgb_array, alphas = ImageConvert.matrix_to_rgb_palette_and_alphas(reshaped_net_output, palette)
 
 
 """Initialise a pygame window and draw the sprite."""
+screen = pygame.display.set_mode((500, 800))
+screen.fill((0, 0, 0))
+
+# TODO: Figure out alphas and surface locking.
+draw_surface_1 = pygame.surface.Surface(SPRITE_DIMENSIONS)
+pygame.surfarray.blit_array(draw_surface_1, rgb_array)
+screen.blit(draw_surface_1, (10, 30))
+
+draw_surface_2 = pygame.surface.Surface(SPRITE_DIMENSIONS)
+pygame.surfarray.blit_array(draw_surface_2, rgb_array)
 
 
-def draw_example(array: np.ndarray, maximum_frames=None) -> None:
-    screen = pygame.display.set_mode((200, 120))
-    running = True
-    frame_counter = 0
-    while running:
-        if maximum_frames is not None:
-            frame_counter += 1
-            if frame_counter >= maximum_frames:
-                running = False
+class MutateSurface:
+    """Collection of impure functions operating on Surface objects."""
 
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                running = False
-                break
+    def set_alphas(surface: pygame.surface.Surface, alphas: np.ndarray) -> None:
+        """Access the alpha values of a Surface and set them to the values in the supplied array.
 
-        if running:  # This if statement prevents a segfault from occuring when closing the pygame window.
-            screen.fill((0, 0, 0))
-            surfarray.blit_array(screen, array)
-            pygame.display.flip()
+        Keeping the reference to the surface contained inside the functions scope results in the surface being unlocked
+        so that it an be subseuently used with the blit method.  Otherwise the surface is locked while the reference
+        array exists.
+        """
+        surface_alphas = pygame.surfarray.pixels_alpha(surface)
+        surface_alphas[:] = alphas[:]
+
+
+MutateSurface.set_alphas(draw_surface_2, alphas)
+
+
+screen.blit(draw_surface_2, (50, 30))
+
+
+pygame.display.flip()
